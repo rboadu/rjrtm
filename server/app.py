a3de3f1fe9b4be151f17323045dfbd0338a033b4
Jaden Ritchie <ritchiejaden801@gmail.com>
2025-10-10 18:07:03 -0400

    updated app.py

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import logging
 
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -8,10 +9,12 @@ logger.info("Server starting…")
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def root():
     logger.info("Successful request to '/'")
     return {'status': 'ok', 'service': 'rjrtm-api', 'version': '0.1'}
+
 
 if __name__ == '__main__':
     app.run(debug=True)
