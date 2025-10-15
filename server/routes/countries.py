2cefa28f48aedc6cf5cf4869cfbb1d2cb3de65eb
mikiyas17legesse <mikiyaslegesse21@gmail.com>
2025-10-14 22:47:00 -0400

    feat: refine GET Endpoint for countries + exception handling

--- server/routes/countries.py
+++ server/routes/countries.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort
 import logging
+from pymongo.errors import PyMongoError
+from data.countries import read_all_countries
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -10,5 +12,15 @@ logger.info("Countries blueprint initialized.")
 @countries_bp.route('/', methods=['GET'])
 def get_countries() -> jsonify:
     """Endpoint to get a list of available countries."""
-    logger.info("Successful request to '/countries/'")
-    return jsonify([])
+    try:
+        logger.info("Successful request to '/countries/'")
+        countries = read_all_countries()
+        for country in countries:
+            country['_id'] = str(country['_id']) 
+        return jsonify(countries)
+    except PyMongoError as e:
+        logger.error(f"Database error: {e}")
+        abort(500, f"Database error: {e}")
+    except Exception as e:
+        logger.error(f"Error retrieving countries: {e}")
+        abort(500, f"Error retrieving countries: {e}")
