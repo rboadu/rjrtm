435aa52033b53ca57a9c149594890064dda6e82f
mikiyas17legesse <mikiyaslegesse21@gmail.com>
2025-10-21 20:29:01 -0400

    feat: GET endpoint for info on specific country code

--- server/routes/countries.py
+++ server/routes/countries.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify, abort
 import logging
 from pymongo.errors import PyMongoError
-from data.countries import read_all_countries
+from data.countries import read_all_countries, read_country_by_code
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -24,3 +24,22 @@ def get_countries() -> jsonify:
     except Exception as e:
         logger.error(f"Error retrieving countries: {e}")
         abort(500, f"Error retrieving countries: {e}")
+
+@countries_bp.route('/<string:code>', methods=['GET'])
+def get_country_by_code(code: str) -> jsonify:
+    """Endpoint to get a specific country by its code (e.g., 'UK')."""
+    try:
+        logger.info(f"Request to '/countries/{code}'")
+        country = read_country_by_code(code)
+        if country:
+            country['_id'] = str(country['_id'])
+            return jsonify(country)
+        else:
+            logger.warning(f"Country with code '{code}' not found")
+            abort(404, f"Country with code '{code}' not found")
+    except PyMongoError as e:
+        logger.error(f"Database error: {e}")
+        abort(500, f"Database error: {e}")
+    except Exception as e:
+        logger.error(f"Error retrieving country: {e}")
+        abort(500, f"Error retrieving country: {e}")
