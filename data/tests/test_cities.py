85d46ae112c990ddef54f287b7c047c02bb460a9
terranagai <7nagai03@gmail.com>
2025-10-21 22:40:33 -0400

    Add test_get_all_cities_with_fake_db to test retrieving all cities using a fake in-memory database

--- data/tests/test_cities.py
+++ data/tests/test_cities.py
@@ -40,3 +40,90 @@ def test_delete_city(sample_city):
     dc.add_city(sample_city)
     success = dc.delete_city("Tokyo")
     assert success
+
+
+def test_get_all_cities_with_fake_db(monkeypatch):
+    """Test get_all_cities using a fake in-memory DB to avoid real Mongo."""
+
+    class FakeCollection:
+        def __init__(self):
+            self._docs = []
+
+        def find(self, *args, **kwargs):
+            # Return an iterator of docs
+            return iter(self._docs)
+
+        def find_one(self, filt, *args, **kwargs):
+            for d in self._docs:
+                match = True
+                for k, v in filt.items():
+                    if d.get(k) != v:
+                        match = False
+                        break
+                if match:
+                    return d
+            return None
+
+        def insert_one(self, doc):
+            # mimic pymongo InsertOneResult by returning the doc
+            self._docs.append(dict(doc))
+            class R:
+                pass
+            return R()
+
+        def update_one(self, filt, update):
+            for d in self._docs:
+                match = True
+                for k, v in filt.items():
+                    if d.get(k) != v:
+                        match = False
+                        break
+                if match:
+                    # apply $set
+                    for k, v in update.get('$set', {}).items():
+                        d[k] = v
+                    class R:
+                        modified_count = 1
+                    return R()
+            class R:
+                modified_count = 0
+            return R()
+
+        def delete_one(self, filt):
+            for i, d in enumerate(self._docs):
+                match = True
+                for k, v in filt.items():
+                    if d.get(k) != v:
+                        match = False
+                        break
+                if match:
+                    del self._docs[i]
+                    class R:
+                        deleted_count = 1
+                    return R()
+            class R:
+                deleted_count = 0
+            return R()
+
+    class FakeDB:
+        def __init__(self):
+            self.cities = FakeCollection()
+
+    fake_db = FakeDB()
+
+    # pre-populate fake DB
+    fake_docs = [
+        {"name": "Tokyo", "country": "Japan", "population": 14000000},
+        {"name": "Osaka", "country": "Japan", "population": 2700000},
+    ]
+    for d in fake_docs:
+        fake_db.cities.insert_one(d)
+
+    # monkeypatch the module-level db used by data.cities
+    monkeypatch.setattr(dc, 'db', fake_db)
+
+    all_cities = dc.get_all_cities()
+    assert isinstance(all_cities, list)
+    assert len(all_cities) == 2
+    names = {c['name'] for c in all_cities}
+    assert names == {"Tokyo", "Osaka"}
